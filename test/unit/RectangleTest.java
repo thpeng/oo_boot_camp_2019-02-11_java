8fa8098ffdfcd09d7ba043c6b56ae323b0bf5557
Fred George <fredgeorge@acm.org>
2019-02-07 14:21:12 +0100

    Fred: Skeleton project with just a RectangleTest

---
+++ test/unit/RectangleTest.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2019 by Fred George
+ * May be used freely except for training; license required for training.
+ */
+
+package unit;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+// Ensures Rectangle operates correctly
+class RectangleTest {
+
+    @Test void area() {
+        assertEquals(24.0, new Rectangle(4.0, 6.0));
+    }
+}
