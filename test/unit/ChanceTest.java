31bed712b82cb941bb8194c4cacb295a1b20bc54
Fred George <fredgeorge@acm.org>
2019-02-11 11:43:00 +0100

    Fred: Added convenience values to ChanceTest; checked that equals never throws an exception

--- test/unit/ChanceTest.java
+++ test/unit/ChanceTest.java
@@ -12,8 +12,16 @@ import static org.junit.jupiter.api.Assertions.*;
 
 // Ensures Chance operates correctly
 class ChanceTest {
+    private static final Chance IMPOSSIBLE = new Chance(0.0);
+    private static final Chance UNLIKELY = new Chance(0.25);
+    private static final Chance EQUALLY_LIKELY = new Chance(0.5);
+    private static final Chance LIKELY = new Chance(0.75);
+    private static final Chance CERTAIN = new Chance(1.0);
 
     @Test void equality() {
-        assertEquals(new Chance(0.75), new Chance(0.75));
+        assertEquals(LIKELY, new Chance(0.75));
+        assertNotEquals(LIKELY, UNLIKELY);
+        assertNotEquals(LIKELY, new Object());
+        assertNotEquals(LIKELY, null);
     }
 }
