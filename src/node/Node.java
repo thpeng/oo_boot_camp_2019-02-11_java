f28316fff96965ac43dcf9d1daef56ec321634f6
u225734 <thierry.peng@sbb.ch>
2019-02-14 13:48:40 +0100

    day4 till midday

--- src/node/Node.java
+++ src/node/Node.java
@@ -6,6 +6,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Node {
 
@@ -15,16 +16,21 @@ public class Node {
         this.name = name;
     }
 
-    private List<Node> targets = new ArrayList<>();
+    private List<Edge> edges = new ArrayList<>();
 
     public void addTarget(Node... other) {
-        targets.addAll(Arrays.asList(other));
+        edges.addAll(Arrays.stream(other)
+                .map(node -> new Edge(this, node, 1))
+                .collect(Collectors.toList()));
+    }
+
+    public void addTarget(Node target, int cost) {
+        edges.add(new Edge(this, target, cost));
     }
 
     public boolean canReach(Node destination) {
-        return hopCount(destination) != -1;
+        return hopCount(destination,  new HashSet<>()) != Double.POSITIVE_INFINITY;
     }
-
 
     @Override
     public boolean equals(Object o) {
@@ -39,33 +45,53 @@ public class Node {
         return Objects.hash(name);
     }
 
-    public int hopCount(Node b) {
-        return hopCount(b, new HashSet<>());
+    public int hopCount(Node destination) {
+        double hopCount = hopCount(destination, new HashSet<>());
+        if(hopCount == Double.POSITIVE_INFINITY) {
+            throw new IllegalArgumentException(destination + " is not reachable");
+        }
+        return (int) hopCount;
     }
 
-    private int hopCount(Node destination, Set<Node> visited) {
+    double hopCount(Node destination, Set<Node> visited) {
         if (this.equals(destination)) {
             return 0;
         }
         if (visited.contains(this)) {
-            return -1;
+            return Double.POSITIVE_INFINITY;
         }
-        visited.add(this);
+        Set<Node> visited2 = new HashSet<>(visited);
+        visited2.add(this);
 
-        /*return targets.stream()
-                .map(node -> node.hopCount(node, visited))
-                .filter(res -> res != -1)
-                .findFirst()
-                .map(hops -> hops +1)
-                .orElse(-1);*/
-        //conservative approach
-        for (Node target : targets) {
-            int result = target.hopCount(destination, visited);
-            if (result != -1) {
-                return result + 1;
-            }
+        return edges.stream()
+                .mapToDouble(edge -> edge.hopCount(destination, visited2))
+                .filter(res -> res != Double.POSITIVE_INFINITY)
+                .min()
+                .orElse(Double.POSITIVE_INFINITY);
+    }
+
+    public double cost(Node destination) {
+        double cost = cost(destination, new HashSet<>());
+        if(cost == Double.POSITIVE_INFINITY) {
+            throw new IllegalArgumentException(destination + " is not reachable");
         }
+        return cost;
+    }
 
-        return -1;
+    double cost(Node destination, Set<Node> visited) {
+        if (this.equals(destination)) {
+            return 0;
+        }
+        if (visited.contains(this)) {
+            return Double.POSITIVE_INFINITY;
+        }
+        Set<Node> visited2 = new HashSet<>(visited);
+        visited2.add(this);
+
+        return edges.stream()
+                .mapToDouble(edge -> edge.cost(destination, visited2))
+                .filter(res -> res != Double.POSITIVE_INFINITY)
+                .min()
+                .orElse(Double.POSITIVE_INFINITY);
     }
 }
---
+++ test/unit/NodeTest.java
@@ -0,0 +1,109 @@
+package unit;
+
+import node.Node;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class NodeTest {
+
+    private static final Node A = new Node("A");
+    private static final Node B = new Node("B");
+    private static final Node C = new Node("C");
+    private static final Node D = new Node("D");
+    private static final Node E = new Node("E");
+    private static final Node F = new Node("F");
+    private static final Node G = new Node("G");
+
+    @BeforeAll
+    public static void init() {
+        B.addTarget(A, 5);
+        B.addTarget(C, 6);
+        B.addTarget(F, 4);
+        C.addTarget(D, 1);
+        C.addTarget(D, 7);
+        C.addTarget(E, 8);
+        D.addTarget(E, 2);
+        E.addTarget(B, 3);
+    }
+
+    @Test
+    void cost() {
+        assertEquals(9, B.cost(E));
+        assertEquals(1, C.cost(D));
+        assertEquals(7, E.cost(F));
+        assertEquals(0, G.cost(G));
+        assertThrows(IllegalArgumentException.class, ()-> A.cost(F));
+    }
+
+    @Test
+    void hopCount() {
+        assertEquals(0, C.hopCount(C));
+        assertEquals(2, C.hopCount(B));
+        assertThrows(IllegalArgumentException.class, ()-> C.hopCount(G));
+        assertEquals(1, B.hopCount(F));
+    }
+
+    @Test
+    void testReach() {
+        assertTrue(A.canReach(A));
+        assertFalse(A.canReach(B));
+        assertFalse(A.canReach(C));
+        assertFalse(A.canReach(D));
+        assertFalse(A.canReach(E));
+        assertFalse(A.canReach(F));
+        assertFalse(A.canReach(G));
+
+        assertTrue(B.canReach(A));
+        assertTrue(B.canReach(B));
+        assertTrue(B.canReach(C));
+        assertTrue(B.canReach(D));
+        assertTrue(B.canReach(E));
+        assertTrue(B.canReach(F));
+        assertFalse(B.canReach(G));
+
+        assertTrue(C.canReach(A));
+        assertTrue(C.canReach(B));
+        assertTrue(C.canReach(C));
+        assertTrue(C.canReach(D));
+        assertTrue(C.canReach(E));
+        assertTrue(C.canReach(F));
+        assertFalse(C.canReach(G));
+
+        assertTrue(D.canReach(A));
+        assertTrue(D.canReach(B));
+        assertTrue(D.canReach(C));
+        assertTrue(D.canReach(D));
+        assertTrue(D.canReach(E));
+        assertTrue(D.canReach(F));
+        assertFalse(D.canReach(G));
+
+        assertTrue(E.canReach(A));
+        assertTrue(E.canReach(B));
+        assertTrue(E.canReach(C));
+        assertTrue(E.canReach(D));
+        assertTrue(E.canReach(E));
+        assertTrue(E.canReach(F));
+        assertFalse(E.canReach(G));
+
+        assertFalse(F.canReach(A));
+        assertFalse(F.canReach(B));
+        assertFalse(F.canReach(C));
+        assertFalse(F.canReach(D));
+        assertFalse(F.canReach(E));
+        assertTrue(F.canReach(F));
+        assertFalse(F.canReach(G));
+
+        assertFalse(G.canReach(A));
+        assertFalse(G.canReach(B));
+        assertFalse(G.canReach(C));
+        assertFalse(G.canReach(D));
+        assertFalse(G.canReach(E));
+        assertFalse(G.canReach(F));
+        assertTrue(G.canReach(G));
+    }
+}
